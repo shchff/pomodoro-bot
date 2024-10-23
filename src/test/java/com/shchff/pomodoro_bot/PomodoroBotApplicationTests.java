f7aa5ffc1b4874a102675101e8e473e817c57f43
shchff <grisaserbakov0@gmail.com>
2024-10-23 22:33:16 +0500

    first commit

---
+++ src/test/java/com/shchff/pomodoro_bot/PomodoroBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shchff.pomodoro_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PomodoroBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
