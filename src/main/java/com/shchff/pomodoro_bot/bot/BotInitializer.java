4212f5c206dfc2a73bae71b19db282a0aab2fec8
shchff <grisaserbakov0@gmail.com>
2024-10-24 11:03:51 +0500

    Changed bot configuration

---
+++ src/main/java/com/shchff/pomodoro_bot/bot/BotInitializer.java
@@ -0,0 +1,19 @@
+package com.shchff.pomodoro_bot.bot;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+@Configuration
+public class BotInitializer
+{
+    @Bean
+    public TelegramBotsApi telegramBotsApi(PomodoroTelegramBot bot) throws TelegramApiException
+    {
+        var api = new TelegramBotsApi(DefaultBotSession.class);
+        api.registerBot(bot);
+        return api;
+    }
+}
--- src/main/java/com/shchff/pomodoro_bot/bot/PomodoroTelegramBot.java
+++ src/main/java/com/shchff/pomodoro_bot/bot/PomodoroTelegramBot.java
@@ -1,8 +1,8 @@
 package com.shchff.pomodoro_bot.bot;
 
 import com.shchff.pomodoro_bot.command.CommandContainer;
-import com.shchff.pomodoro_bot.config.BotConfig;
 import com.shchff.pomodoro_bot.service.SendBotMessageServiceImpl;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.objects.Update;
@@ -15,12 +15,14 @@ public class PomodoroTelegramBot extends TelegramLongPollingBot
     public static String COMMAND_PREFIX = "/";
 
     private final CommandContainer commandContainer;
-    private final BotConfig config;
 
-    public PomodoroTelegramBot(BotConfig config)
+    @Value("${bot.username}")
+    private  String username;
+
+    public PomodoroTelegramBot(@Value("${bot.token}") String botToken)
     {
+        super(botToken);
         this.commandContainer = new CommandContainer(new SendBotMessageServiceImpl(this));
-        this.config = config;
     }
 
     @Override
@@ -43,12 +45,6 @@ public class PomodoroTelegramBot extends TelegramLongPollingBot
     @Override
     public String getBotUsername()
     {
-        return config.getUsername();
-    }
-
-    @Override
-    public String getBotToken()
-    {
-        return config.getToken();
+        return username;
     }
 }
